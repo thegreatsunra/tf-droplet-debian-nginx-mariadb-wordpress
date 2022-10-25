035feeedf2dd199af5b7dbc29db11ee247271734
Dane Petersen <thegreatsunra@gmail.com>
2022-10-24 20:19:15 -0500

    Don't deploy DNS for now

--- deploy/dns.tf
+++ deploy/dns.tf
@@ -1,18 +1,19 @@
 ## each.key is the full domain name, with subdomain if applicable
 ## each.value is the zone, without zubdomain
 ## this way the keys are always unique, even if they share the same domain
-data "cloudflare_zone" "zone" {
-  for_each = local.container_loop.dns
 
-  name = each.value
-}
+# data "cloudflare_zone" "zone" {
+#   for_each = local.container_loop.dns
 
-resource "cloudflare_record" "record" {
-  for_each = local.container_loop.dns
+#   name = each.value
+# }
 
-  name    = each.key
-  proxied = true
-  type    = "A"
-  value   = digitalocean_droplet.droplet.ipv4_address
-  zone_id = data.cloudflare_zone.zone[each.key].id
-}
+# resource "cloudflare_record" "record" {
+#   for_each = local.container_loop.dns
+
+#   name    = each.key
+#   proxied = true
+#   type    = "A"
+#   value   = digitalocean_droplet.droplet.ipv4_address
+#   zone_id = data.cloudflare_zone.zone[each.key].id
+# }
